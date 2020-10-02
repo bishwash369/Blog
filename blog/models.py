d3f7de0bce21014994dded85e9cdca15216bf283
bishwashparajuli10 <bishwashparajuli10@gmail.com>
2020-10-02 20:23:10 +0545

    Database models updated for Blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # default = timezone.now lets you update the date
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if user deleted then post gets deleted as well
+
+    def __str__(self):
+        return self.title
